33644feb050deee6531b817a7566961fe0e73809
Paul Tetreau <paul@blacksheepwall.com>
2015-06-26 23:05:19 -0400

    more karshan

--- src/hhttpp.hs
+++ src/hhttpp.hs
@@ -1,58 +1,67 @@
 import Text.ParserCombinators.Parsec
 
-data HTTPRequest = HTTPRequest { _verb :: String, _path :: String, request_version :: String, request_body :: HTTPBody } deriving Show
+data HTTPRequest = HTTPRequest { verb :: String, path :: String, request_version :: String, query_params :: [(String, String)], request_body :: HTTPBody } deriving Show
 
-data HTTPResponse = HTTPResponse { _response_version :: String, _status_code :: Int,  _status_msg :: String, response_body :: HTTPBody } deriving Show
+data HTTPResponse = HTTPResponse { response_version :: String, status_code :: Int,  status_msg :: String, response_body :: HTTPBody } deriving Show
 
 data HTTPBody = HTTPBody { } deriving Show
 
 -- TODO properly support \r\n
-http_request :: GenParser Char st HTTPRequest
+http_request :: Parser HTTPRequest
 http_request =
-     http_request_verb >>= (\verb ->
-     http_request_path >>= (\path ->
+     http_request_verb >>= (\verb' ->
+     http_request_path >>= (\path' ->
+     http_request_query_params >>= (\qp ->
      -- http_request_parameters
      http_msg_version >>= (\version ->
-     many http_msg_header >>
-     http_msg_body >>
-     end_of_http_msg >>
-     return HTTPRequest { _verb = verb, _path = path, request_version = version, request_body = undefined })))
+     return HTTPRequest { verb = verb', path = path', query_params = qp, request_version = version, request_body = undefined }))))
 
 end_of_http_msg = undefined
-http_request_parameters = undefined
 
-http_first_line_response :: GenParser Char st [[String]]
+http_first_line_response :: Parser [[String]]
 http_first_line_response =
   do http_msg_version
      http_response_status_code
      http_response_status_msg
      return []
 
-http_request_verb :: GenParser Char st String
+http_request_verb :: Parser String
 http_request_verb = many (noneOf " ")
 
-http_request_path :: GenParser Char st String
+http_request_path :: Parser String
 http_request_path = many (noneOf " ?")
 
-http_msg_version :: GenParser Char st String
+http_request_query_params :: Parser [(String, String)]
+http_request_query_params = char '?' >> http_request_parameters
+
+http_request_parameters :: Parser [(String, String)]
+http_request_parameters = many (parse_one_pair <* oneOf "&;")
+    where
+        parse_one_pair :: Parser (String, String)
+        parse_one_pair = many (noneOf " =") >>= (\key ->
+                         many (noneOf " &;") >>= (\val ->
+                         return (key, val)))
+    
+
+http_msg_version :: Parser String
 http_msg_version = many (noneOf " \n")
 
-http_response_status_code :: GenParser Char st Int
+http_response_status_code :: Parser Int
 http_response_status_code = many (noneOf " ") >> return 0
 
-http_response_status_msg :: GenParser Char st [String]
+http_response_status_msg :: Parser [String]
 http_response_status_msg = many (noneOf "\n") >> return []
 
-http_msg_header :: GenParser Char st [String]
+http_msg_header :: Parser [String]
 http_msg_header =
   do key <- http_msg_header_key
      val <- http_msg_header_val
      return [] -- (key : val)
 
-http_msg_header_key :: GenParser Char st [String]
+http_msg_header_key :: Parser [String]
 http_msg_header_key = many (noneOf ":\n") >> return []
 
-http_msg_header_val :: GenParser Char st [String]
+http_msg_header_val :: Parser [String]
 http_msg_header_val = many (noneOf "\n") >> return []
 
 http_msg_body :: GenParser Char st [String]
