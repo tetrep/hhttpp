f499c7a228d34701a14e8710961503e5436fcec4
Karshan Sharma <karshan.sharma@gmail.com>
2015-07-08 20:13:48 -0700

    Remove support for headers without :

--- src/HHTTPP/Common.hs
+++ src/HHTTPP/Common.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 module HHTTPP.Common where
 
-import Control.Monad (join)
 import Data.CaseInsensitive (CI, mk)
 import Data.Maybe (fromMaybe)
 import HHTTPP.Util (maybe_read)
 import Text.Parsec.ByteString (Parser)
 import Text.Parsec
 
-type Header = (CI String, Maybe String)
+type Header = (CI String, String)
 
 consume_spaces :: Parser String
 consume_spaces = many (char ' ')
@@ -19,7 +18,7 @@ consume_eol = string "\r\n" <|> string "\n"
 parse_msg_version :: Parser String
 parse_msg_version = many (noneOf " \r\n")
 
-parse_msg_header :: Parser (CI String, Maybe String)
+parse_msg_header :: Parser (CI String, String)
 parse_msg_header =
   parse_msg_header_key >>= (\key ->
   parse_msg_header_val >>= (\val ->
@@ -27,17 +26,16 @@ parse_msg_header =
   return (key, val)))
 
 parse_msg_header_key :: Parser (CI String)
-parse_msg_header_key = mk <$> many1 (noneOf ":\r\n")
+parse_msg_header_key = mk <$> many (noneOf ":\r\n")
 
--- returns nothing if there wasn't even a colon :O
-parse_msg_header_val :: Parser (Maybe String)
-parse_msg_header_val = option Nothing (fmap Just (char ':' >> consume_spaces >> many (noneOf "\r\n")))
+parse_msg_header_val :: Parser String
+parse_msg_header_val = char ':' >> consume_spaces >> many (noneOf "\r\n")
 
 parse_msg_body :: Int -> Parser String
 parse_msg_body n = count n anyToken
 
-get_content_length :: [(CI String, Maybe String)] -> Maybe Int
-get_content_length headers = maybe_read =<< join (lookup "Content-Length" headers)
+get_content_length :: [Header] -> Maybe Int
+get_content_length headers = maybe_read =<< lookup "Content-Length" headers
 
 parse_headers_and_body:: Parser ([Header], String)
 parse_headers_and_body =
